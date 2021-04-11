c20a3ec1bb95a546a7732b2cfe0c40acd2e69f07
jimccis1105 <jimccis1105@gmail.com>
2021-04-11 22:11:09 +0800

    created server

---
+++ server.js
@@ -0,0 +1,21 @@
+// TODO: visit https://expressjs.com/
+const express = require('express')
+const app = express()
+
+// Configures CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+// const quizzesController = require('./controllers/quizzes-controller')
+// quizzesController(app)
+
+require('./controllers/quizzes-controller')(app)
+require('./controllers/questions-controller')(app)
+
+app.listen(4000)
