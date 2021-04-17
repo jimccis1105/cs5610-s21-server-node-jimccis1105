ebe91860636ccb6807779554cdeaa5ac8056aa0d
jimccis1105 <jimccis1105@gmail.com>
2021-04-17 15:27:44 +0800

    Refactor Services to Use DAOs Instead of JSON Arrays

--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -1,22 +1,27 @@
-const quizzes = require('./quizzes.json')
+const quizzesDao = require('../../daos/quizzes-dao')
+const findAllQuizzes = () => quizzesDao.findAllQuizzes()
+const findQuizById = (qid) => quizzesDao.findQuizById(qid)
+module.exports = { findAllQuizzes, findQuizById }
 
-// TODO: Node.js Assignment this week
-const findAllQuizzes = () => {
-    return quizzes
-}
-const findQuizById = (quizId) => {
-    return quizzes.find((quiz) => {
-        return quiz._id === quizId
-    })
-}
-
-// TODO: MongoDB Assignment next week
-const createQuiz = () => {}
-const updateQuiz = () => {}
-const deleteQuiz = () => {}
-
-module.exports = {
-    createQuiz,
-    findQuizById, findAllQuizzes,
-    updateQuiz, deleteQuiz
-}
+// const quizzes = require('./quizzes.json')
+//
+// // TODO: Node.js Assignment this week
+// const findAllQuizzes = () => {
+//     return quizzes
+// }
+// const findQuizById = (quizId) => {
+//     return quizzes.find((quiz) => {
+//         return quiz._id === quizId
+//     })
+// }
+//
+// // TODO: MongoDB Assignment next week
+// const createQuiz = () => {}
+// const updateQuiz = () => {}
+// const deleteQuiz = () => {}
+//
+// module.exports = {
+//     createQuiz,
+//     findQuizById, findAllQuizzes,
+//     updateQuiz, deleteQuiz
+// }
